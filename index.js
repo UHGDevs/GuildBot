459daea187514bb6ac3955ccc60bcd27698cbe8f
petrkolencik <petr.kolencik@gooddata.com>
2022-03-06 18:52:59 +0100

    edited index

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const fs = require('fs');
 const mineflayer = require('mineflayer');
-console.log(mineflayer)
+const { Client, Intents, Collection, Util, WebhookClient, MessageEmbed } = require('discord.js');
+
+const uhg = {}
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+uhg.client = new Client({
+  allowedMentions: { parse: [ "users", "roles" ], repliedUser: true },
+  intents: [Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
+});
+
+uhg.mccommands = new Collection();
+uhg.mcaliases = new Collection();
+
+console.log("POG")
+
+uhg.client.login(process.env.token);
