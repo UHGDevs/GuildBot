06949de5dfd5d79662f8244d515a30bdec33e3bf
petrkolencik <petr.kolencik@gooddata.com>
2022-03-06 19:44:36 +0100

    started adding mc

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ const minecraft = require('minecraft-protocol');
 const colors = require("colors");
 const { Client, Intents, Collection, Util, WebhookClient, MessageEmbed } = require('discord.js');
 
-const uhg = {}
-
 const dotenv = require('dotenv');
 dotenv.config();
 
 
+let uhg = {}
 uhg.dc = {}
 uhg.dc.client = new Client({
   allowedMentions: { parse: [ "users", "roles" ], repliedUser: true },
   intents: [Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
 });
-uhg.mc = {}
-uhg.mc.commands = new Collection();
-uhg.mc.aliases = new Collection();
 
 uhg.dc.client.on('ready', () => {
   console.log(`Discord Bot is online!`.bold.brightGreen)
@@ -28,11 +24,21 @@ uhg.dc.client.on('error', (e) => {
   console.log(String(e).red.dim);
 });
 
-require(`./minecraft/commands`)(uhg.dc.client);
-// const client = minecraft.createClient({
-//   host: "mc.hypixel.net",
-//   username: process.env.email,
-//   auth: 'microsoft'
+uhg.mc = {}
+uhg.mc.commands = new Collection();
+uhg.mc.aliases = new Collection();
+
+require(`./minecraft/commands`)(uhg);
+
+// uhg.mc.client = minecraft.createClient({
+//   host: "play.survival-games.cz",
+//   //host: "mc.hypixel.net",
+//   username: "dasfhvas",
+//   //username: process.env.email,
+//   //password: process.env.password,
+//   //auth: 'microsoft'
 // });
+
+
 
 uhg.dc.client.login(process.env.token);
