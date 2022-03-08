48d70d4d30e74ad13726a4948560efd7ef04e555
petrkolencik <petr.kolencik@gooddata.com>
2022-03-08 21:21:10 +0100

    added class

--- index.js
+++ index.js
@@ -7,38 +7,63 @@ const { Client, Intents, Collection, Util, WebhookClient, MessageEmbed } = requi
 const dotenv = require('dotenv');
 dotenv.config();
 
+const config = require('./settings/config.json');
 
-let uhg = {}
-uhg.dc = {}
-uhg.dc.client = new Client({
-  allowedMentions: { parse: [ "users", "roles" ], repliedUser: true },
-  intents: [Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
-});
+class login {
+  constructor(dc, mc) {
+    this.dc = {client: dc}
+    this.mc = {client: mc}
+    this.settings = JSON.parse(fs.readFileSync('settings/config.json', 'utf8'));
+  }
 
-uhg.dc.client.on('ready', () => {
-  console.log(`Discord Bot is online!`.bold.brightGreen)
-  console.log(`${uhg.dc.client.user.tag}`.bold.brightGreen)
-	uhg.dc.client.user.setActivity(' Guild Chat', { type: 'WATCHING' });
-});
-uhg.dc.client.on('error', (e) => {
-  console.log(String(e).red.dim);
-});
-
-uhg.mc = {}
-uhg.mc.commands = new Collection();
-uhg.mc.aliases = new Collection();
-
-require(`./minecraft/commands`)(uhg);
-
-// uhg.mc.client = minecraft.createClient({
-//   host: "play.survival-games.cz",
-//   //host: "mc.hypixel.net",
-//   username: "dasfhvas",
-//   //username: process.env.email,
-//   //password: process.env.password,
-//   //auth: 'microsoft'
-// });
+  reload(reload="all") {
+    if (reload == "settings" || reload == "all") {
+      this.settings = JSON.parse(fs.readFileSync('settings/config.json', 'utf8'));
+    }
+  }
 
 
+}
 
-uhg.dc.client.login(process.env.token);
+let { dc, mc } = "UHG"
+
+if (config.discord === true) {
+  dc = new Client({
+    allowedMentions: { parse: [ "users", "roles" ], repliedUser: true },
+    intents: [Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
+  });
+}
+
+if (config.minecraft === true) {
+  mc = minecraft.createClient({
+    host: "play.survival-games.cz",
+    //host: "mc.hypixel.net",
+    username: "dasfhvas",
+    //username: process.env.email,
+    //password: process.env.password,
+    //auth: 'microsoft'
+  });
+}
+
+let uhg = new login(dc, mc)
+
+setInterval(function () {
+  console.log(uhg.settings)
+  uhg.reload()
+}, 5000);
+
+
+if (dc) {
+  uhg.dc.client.on('ready', () => {
+    console.log(`Discord Bot is online!`.bold.brightGreen)
+    console.log(`${uhg.dc.client.user.tag}`.bold.brightGreen)
+  	uhg.dc.client.user.setActivity(' Guild Chat', { type: 'WATCHING' });
+  });
+  uhg.dc.client.on('error', (e) => {
+    console.log(String(e).red.dim);
+  });
+
+
+
+  uhg.dc.client.login(process.env.token);
+}
