d34f9cc47b728fc83c16758dd4717870745c15c6
DavidCzPdy <dakolencik@gmail.com>
2022-03-09 19:05:16 +0100

    started test messages

--- discord/message.js
+++ discord/message.js
@@ -2,5 +2,10 @@ module.exports = async (uhg, message) => {
   if (!message.guild || !message.channel || message.author.bot) return;
   if (message.channel.partial) await message.channel.fetch();
   if (message.partial) await message.fetch();
- await uhg.test.server.broadcast(`§2Guild > §a[VIP§6+§a] The_AntiFrost_SK&f: necham drakov`);
+
+  if (!message.content.startsWith(".")) return
+  let content = message.content.replace(".", "")
+
+  if (content = "gchat") await uhg.test.server.broadcast(`§2Guild > §a[VIP§6+§a] The_AntiFrost_SK&f: necham drakov`);
+  if (content = "pchat") await uhg.test.server.broadcast(`§2Party > §a[VIP§6+§a] The_AntiFrost_SK&f: necham drakov`);
 }
