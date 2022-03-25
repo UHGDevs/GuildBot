9754ede38f7ebb0d6a7998f1b58c072d32e7e79d
DavidCzPdy <dakolencik@gmail.com>
2022-03-25 15:03:22 +0100

    antispam is here!

--- minecraft/send.js
+++ minecraft/send.js
@@ -1,6 +1,6 @@
 exports.send = send
 async function send(uhg, pmsg) {
-  let message = pmsg.send;
+  let message = pmsg.send.slice(0, 256);
   if (!message) return
   if (pmsg.count && pmsg.count > 2) return console.log("MOC POKUSŮ NA POSLÁNÍ ZPRÁVY")
 
@@ -11,7 +11,7 @@ async function send(uhg, pmsg) {
     let channel = pmsg.channel || ""
 
     if (message.startsWith("/")) pmsg.ready = true
-    if (channel.startsWith("/")) pmsg.ready = true
+    else if (channel.startsWith("/")) {message = channel.trim() + " " + message; pmsg.ready = true;}
   }
 
   if (!pmsg.ready) {
@@ -36,10 +36,11 @@ async function send(uhg, pmsg) {
     // TODO: SEND ERROR HANDLERING (like you cannot send this message twice / you cannot message this player etc.)
 
     if (pmsg.error == "You cannot say the same message twice!") {
+      pmsg.antispam = " [" + ((Math.random()+1).toString(36).substring(2)+(Math.random()+1).toString(36).substring(2)+(Math.random()+1).toString(36).substring(2)+(Math.random()+1).toString(36).substring(2)).slice(0, Math.ceil((message.length)/100*15)) + "]"
 
-    }
-    else if (pmsg.error == "You cannot message this player.") {
+    } else if (pmsg.error == "You cannot message this player.") {
       console.log(pmsg.send)
+      console.log(`${pmsg.username} má vyplé msg!`)
       // Pokud to je že neni verified tak poslat na dc, jinak return
     }
   }
@@ -50,13 +51,16 @@ async function send(uhg, pmsg) {
   else pmsg.count += 1
 
   uhg.mc.send.push(pmsg)
-  console.log(uhg.mc.send)
+  // console.log(uhg.mc.send)
   return
 }
 
 exports.write = async function (uhg, pmsg) {
   try {
-    let msg = pmsg.send + (pmsg.antispam || "")
+    let msg = pmsg.send
+    if (pmsg.antispam && msg.length > (256-pmsg.antispam.length)) msg = msg.slice(0, 253-pmsg.antispam.length)+"..." + pmsg.antispam
+    else if (!pmsg.antispam && msg.length > 256) msg = msg.slice(0, 253)+"..."
+    else if (pmsg.antispam) msg = msg + pmsg.antispam
 
     uhg.mc.client.write("chat", { message: msg, position: 0 })
 
@@ -68,7 +72,6 @@ exports.write = async function (uhg, pmsg) {
     })
     await delay(500)
     if (!pmsg.error) return
-
     send(uhg, pmsg)
   } finally {return uhg.mc.ready = true}
 }
