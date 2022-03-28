ade98fa345ea7375a19501c128226d262863bd12
DavidCzPdy <hrydakol@gmail.com>
2022-03-28 14:56:45 +0200

    guild finder

--- minecraft/events/guild.js
+++ minecraft/events/guild.js
@@ -1,6 +1,8 @@
 let bridge = require(`../bridge.js`)
 let chat = require(`../send.js`)
 module.exports = async (uhg, pmsg) => {
+  let finder = pmsg.msg.match(/^Guild > (\[.*]\s*)?([\w]{2,17}).*?(\[.{1,15}])?: (někdo|nekdo|someone|any|[0-9]\/[0-9]|[0-9])? (\w{2,10})?(.*)$/)
+  if (finder) guildfinder(uhg, pmsg, finder)
   if (pmsg.msg.match(/^Guild > (\[.*]\s*)?([\w]{2,17}).*?(\[.{1,15}])?: (.*)$/)) await bridge.chat(uhg, pmsg)
   else await bridge.info(uhg, pmsg)
 
@@ -15,4 +17,15 @@ module.exports = async (uhg, pmsg) => {
   await bridge.send(uhg, pmsg.send) //await uhg.dc.channels.botjs.send(pmsg.command + " - command comming soon")
 
   await chat.send(uhg, pmsg)
+}
+
+async function guildfinder(uhg, pmsg, finder) {
+  let user = await uhg.mongo.run.get("general", "guildfind", {_id: pmsg.username}) || {}
+  if (user) user = user[0]
+  let games = user.games || [];
+  let messages = user.messages || [];
+
+  games.push(finder[5])
+  messages.push(pmsg.content)
+  uhg.mongo.run.post("general", "guildfind", {_id: pmsg.username, discord: pmsg.id, games: games, messages: messages, updated: Number(new Date())})
 }
---
+++ time/events/finder.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "finder",
+  description: "Find and play!",
+  time: '0 * * * * *', //'*/10 * * * * *'
+  ignore: '* * * * * *', //'sec min hour den(mesic) mesic den(tyden)'
+  run: async (uhg) => {
+    try {
+      let data = await uhg.mongo.run.get("general", "guildfind")
+      data.forEach(item => {
+        if (item.updated+1000*60*60<Number(new Date())) uhg.mongo.run.delete("general", "guildfind", {_id: item._id})
+      });
+      return
+    } catch(e) {
+        console.log(String(e.stack).bgRed)
+        return "Chyba v finder refresh!"
+    }
+  }
+}
