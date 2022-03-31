70a8c6fc3abb8d28ca6c5ceb0fba65b8be915026
DavidCzPdy <hrydakol@gmail.com>
2022-03-31 18:48:02 +0200

    fix

--- discord/commands/getunverified.js
+++ discord/commands/getunverified.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "getunverified",
   aliases: ["getun"],
-  allowedids: ["959129385993125888", "312861502073995265", "379640544143343618", "427198829935460353"],
+  allowedids: ["959129385993125888", "312861502073995265", "379640544143343618", "427198829935460353", "378928808989949964"],
   platform: "dc",
   run: async (uhg, message, content) => {
     try {
@@ -13,13 +13,14 @@ module.exports = {
         return send.join("\n")
       }
 
-      let nickname = args[1]
+      let nickname = args[0]
+      console.log(uhg.data.unverified)
       let player = uhg.data.unverified.filter(n => n.name.toLowerCase()==nickname.toLowerCase())
       if (!player.length) return "Hráč nebyl nalezen"
       return `${player[0].name} - ${player[0].joined}D`
     } catch (e) {
         console.log(String(e.stack).bgRed)
-        return "Chyba ve verify příkazu!"
+        return "Chyba v get unverified příkazu!"
     }
   }
 }
