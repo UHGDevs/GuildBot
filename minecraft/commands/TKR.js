8a7a6bfb7990a210adf163673ccd4797303ee36a
Farmans <Farman52CZ@seznam.cz>
2022-04-18 08:11:58 +0200

    update

--- minecraft/commands/TKR.js
+++ minecraft/commands/TKR.js
@@ -7,7 +7,7 @@ module.exports = {
       let api = await uhg.getApi(nickname)
       if (api instanceof Object == false) return api
       let tkr = api.hypixel.stats.tkr
-      let message = `**TKR**: [${tkr.gold}✪] **${api.username}** - ${tkr.wins}Wins (Trophies: Gold - ${tkr.gold}, Silver - ${tkr.silver}, Bronze - ${tkr.bronze})`
+      let message = `**TKR**: [${uhg.f(tkr.gold)}✪] **${api.username}** - ${uhg.f(tkr.trophies)} Trophies (Gold - ${uhg.f(tkr.gold)}, Silver - ${uhg.f(tkr.silver)}, Bronze - ${uhg.f(tkr.bronze)}) ${uhg.f(tkr.wlr)}WLR ${uhg.f(tkr.trophyratio)} Trophy Ratio (Best Rt: ${uhg.f(tkr.highestrt)}, Best Pos: #${tkr.highestpos})`
       return message
     } catch (e) {
         console.log(String(e.stack).bgRed)
