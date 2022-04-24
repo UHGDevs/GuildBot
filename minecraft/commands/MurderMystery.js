281c402cb612df9d860f94499b3640acb0370667
Farmans <Farman52CZ@seznam.cz>
2022-04-24 10:00:40 +0200

    Update MurderMystery.js

--- minecraft/commands/MurderMystery.js
+++ minecraft/commands/MurderMystery.js
@@ -8,7 +8,7 @@ module.exports = {
       let murder = api.hypixel.stats.murder
       let overall = murder.overall
       let nickname = `**${api.username}**`
-      let message = `**MurderMystery**: ${nickname} - ${uhg.uhg.f(overall.wins)}Wins (M: ${uhg.f(murder.murdererwins)} D: ${uhg.f(murder.detectivewins)} H: ${uhg.f(murder.herowins)}) ${f(overall.kills)}Kills`
+      let message = `**MurderMystery**: ${nickname} - ${uhg.f(overall.wins)}Wins (M: ${uhg.f(murder.murdererwins)} D: ${uhg.f(murder.detectivewins)} H: ${uhg.f(murder.herowins)}) ${uhg.f(overall.kills)}Kills (Main gamemode - ${murder.main_mode})`
       return message
     } catch (e) {
         console.log(String(e.stack).bgRed)
