3add5cb47157b6b49e6004e74c14b406e7383296
DavidCzPdy <dakolencik@gmail.com>
2022-04-27 15:57:57 +0200

    cakes update

--- minecraft/commands/cakes.js
+++ minecraft/commands/cakes.js
@@ -8,12 +8,14 @@ module.exports = {
       if (!args.length) return '!cakes on|off'
 
       if (args[0] == 'off') {
-        return
+        uhg.mongo.run.update('general', 'uhg', { username: pmsg.username }, { cakes: {} })
+        return 'Už nebudeš dostávat cakes upozornění'
       } else if (args[0] != 'on') return '!cakes on|off'
       if (args.length < 2) return '!cakes on [profile]'
 
       let profile = args[1]
       let message = `coming soon (zapínání na profil ${profile})`
+      uhg.mongo.run.update('general', 'uhg', { username: pmsg.username }, { cakes: {toggle: true, profile: profile} })
       return message
     } catch (e) {
         console.log(String(e.stack).bgRed)
--- minecraft/events/cakes.js
+++ minecraft/events/cakes.js
@@ -1,10 +1,11 @@
 let chat = require(`../send.js`)
 let bridge = require(`../bridge.js`)
 module.exports = async (uhg, pmsg) => {
+    let data = pmsg.verify_data.cakes || {}
+    if (!data || !data.toggle) return
+
     let profile = "Grapes";
-    let turn = "on";
-    console.log(pmsg)
-    return
+
     let api = await uhg.getApi(pmsg.username, ["api", "skyblock", "hypixel", "mojang"], ["main"])
     if (api instanceof Object == false) return api
     let cakes = api.skyblock.main[profile].cakes
