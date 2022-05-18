40876721e969e69a71a76850c17c3f16062bfd50
Farmans <Farman52CZ@seznam.cz>
2022-05-18 17:00:30 +0200

    Update Coins.js

--- minecraft/commands/Coins.js
+++ minecraft/commands/Coins.js
@@ -11,16 +11,17 @@ module.exports = {
       let sum = 0;
       let bank = 0;
       let purse = 0;
+      let apioff;
       for (let i=0; i<Object.keys(coins).length; i++) {
-        if (Object.values(coins)[i].sum >= sum) {
+        if (Object.values(coins)[i].bank.sum >= sum) {
           profile = Object.keys(coins)[i]
-          sum = Object.values(coins)[i].sum
-          bank = Object.values(coins)[i].bank
-          purse = Object.values(coins)[i].purse
+          sum = Object.values(coins)[i].bank.sum
+          bank = Object.values(coins)[i].bank.bank
+          purse = Object.values(coins)[i].bank.purse
+          apioff = Object.values(coins)[i].bank.apioff
         }
       }
-      let profil = coins[profile].bank
-      let apioff = profil.apioff
+      let profil = coins[profile]
       if (apioff == true) return `Coins: **${api.username}** - Purse: ${uhg.f(purse).split(".")[0]} (BANK API OFF)`
       console.log(profile)
       let message = `Coins: [${uhg.f(sum).split(".")[0]}] **${api.username}** - Purse: ${uhg.f(purse).split(".")[0]} - Bank: ${uhg.f(bank).split(".")[0]}`
