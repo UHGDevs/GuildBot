76983ca57ee505cfe2e094d7303a30a889a3d88f
Farmans <Farman52CZ@seznam.cz>
2022-06-01 17:06:00 +0200

    čeština v2

--- minecraft/commands/Fact.js
+++ minecraft/commands/Fact.js
@@ -18,14 +18,14 @@ module.exports = {
             `Fact #2 - ${fact2}`,
             `Fact #3 - Momentálně se v Limbu nachází ${uhg.f(api.gamecounts.games.limbo || 0)} hráčů`,
             `Fact #4 - Momentálně na serveru hraje ${uhg.f(api.gamecounts.playerCount || 0)} hráčů`,
-            `Fact #5 - Víte, že MVP++ má na lobby Speed II, ale ostatní ranky jen Speed I? P2W server, confirmed`,
+            `Fact #5 - Víte, že MVP++ má na lobby Speed II, ale ostatní ranky mají jen Speed I? P2W server, confirmed`,
             `Fact #6 - Momentálně na Hypixelu je přes 50 možných her, dokážeš je všechny vymasterovat?`,
             `Fact #7 - Víte, že na SkyBlocku existuje takový Cookie Clicker? Najdete ho v Booster Cookie menu`,
             `Fact #8 - Víte, že když si změníte jazyk na serveru, tak se vám změní nápisy psanými bloky? Například LEAD a INFO na různých lobby`,
             `Fact #9 - ${fact9}`,
             `Fact #10 - Jak mám ksakru testovat tento příkaz, když to trvá 5 let, než se dostanu na ten konkrétní fakt, který chci!!`,
             `Fact #11 - Momentálně ${uhg.f(api.gamecounts.games.skyblock.crimson_isle || 0)} hráčů hraje na Crimson Isle`,
-            `Fact #12 - Momentálně ${uhg.f(api.gamecounts.games.skyblock.dungeons || 0)} hráčů throwují v Dungeonech`,
+            `Fact #12 - Momentálně ${uhg.f(api.gamecounts.games.skyblock.dungeons || 0)} hráčů throwuje v Dungeonech`,
             `Fact #13 - Tipněte si kolik lidí je momentálně v Crystal Hollows.... špatně, haha, je jich tam přesně ${uhg.f(api.gamecounts.games.skyblock.hollows || 0)}`,
         ];
         let randomNumber = Math.random() 
