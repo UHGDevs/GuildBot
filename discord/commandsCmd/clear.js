5044ee6acdf10bbb59a4c4a45cb27f3e5d9d0be3
DavidCzPdy <dakolencik@gmail.com>
2022-06-09 22:00:27 +0200

    format fixes

--- discord/commandsCmd/clear.js
+++ discord/commandsCmd/clear.js
@@ -5,13 +5,13 @@ module.exports = {
   allowedids: [ "419183469911080960", "378928808989949964"],
   allowedroles: [],
   platform: "cmd",
-  queue: { name: 'Clear', value: 'clear', sort: -2 },
+  queue: { name: 'Clear', value: 'clear', sort: 200 },
   run: async (uhg, interaction) => {
     try {
       let amount = interaction.options.getNumber("amount")
-      if (!amount) { return interaction.editReply({ embeds: [new MessageEmbed().setTitle("zadajte cislo").setDescription("q")] })}
-      interaction.channel.bulkDelete(amount, true)
-      interaction.editReply({ embeds: [new MessageEmbed().setTitle(`odstranil si ${amount}`).setDescription("q")] })
+      if (!amount) { return interaction.editReply({ embeds: [new MessageEmbed().setTitle("Missing amount")] })}
+     // interaction.channel.bulkDelete(amount, true)
+      interaction.editReply({ embeds: [new MessageEmbed().setTitle(`(ne)Odstranil jsi ${amount} zpráv`)] })
       //return interaction.editReply({ embeds: [embed] })
     } catch (e) {
         interaction.editReply({ embeds: [uhg.dc.cache.embeds.error(e, 'CLEAR command')] })
