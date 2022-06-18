03ce0a23a6396634f67f2b08a34e74f76255d0e1
Farmans <Farman52CZ@seznam.cz>
2022-06-18 18:02:48 +0200

    Update WoolWars.js

--- minecraft/commands/WoolWars.js
+++ minecraft/commands/WoolWars.js
@@ -9,7 +9,6 @@ module.exports = {
       if (api instanceof Object == false) return api
       let ww = api.hypixel.stats.ww
       let message = `**WoolWars**: [${Math.floor(ww.levelformatted)}✫] ${api.username} - ${f(ww.wins)}Wins ${f(ww.kills)}Kills ${f(ww.wlr)}WLR ${f(ww.kdr)}KDR (${f(Math.round(ww.levelxpleft))} XP do dalšího levelu) | (Most played class based on kills: ${ww.main_class[0]})`
-      //let message = `**WoolWars**: [${Math.floor(ww.lvl.level)}✫] ${api.username} - ${f(ww.wins)}Wins ${f(ww.kills)}Kills ${f(ww.wlr)}WLR ${f(ww.kdr)}KDR (${f(Math.round(ww.xp))} XP) | (Selected Class: ${ww.selected_class})`
       return message
     } catch (e) {
         console.log(String(e.stack).bgRed)
