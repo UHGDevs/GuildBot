6772684299d1b7d72e1a870d8eabdd3ac5fc8062
Farmans <Farman52CZ@seznam.cz>
2022-06-21 18:15:23 +0200

    Update bridge.js

--- discord/bridge.js
+++ discord/bridge.js
@@ -2,11 +2,11 @@ const banned = ["vape", "liquidbounce", "wurst", "sigma", "huzuni", ".org", ".ne
 module.exports = async (uhg, message) => {
   let user = uhg.data.verify.filter(n=>n._id==message.author.id)[0]
   if (!user) return message.reply(`Nejsi verifikovaný, zpráva nebyla odeslána.\nVerifikuj se pomocí ${uhg.settings.prefix}verify *\`nick\`*`)
-  if (message.content == " ") return message.reply("Zpráva nelze odeslat")
-  if (message.content.length>245) message.reply({ content: "Zpráva byla useknuta" })
+  if ((message.channel.id==uhg.getDiscordIds().channels.officer || message.channel.id==uhg.getDiscordIds().channels.guild) && message.content == " ") return message.reply("Zpráva nelze odeslat")
+  if ((message.channel.id==uhg.getDiscordIds().channels.officer || message.channel.id==uhg.getDiscordIds().channels.guild) && message.content.length>245) message.reply({ content: "Zpráva byla useknuta" })
 
   for (let i = 0; i < banned.length; i++) {
-    if (message.content.toLowerCase().includes(banned[i])) return message.reply({ content: "Zpráva obsahuje nepovolené slovo" })
+    if ((message.channel.id==uhg.getDiscordIds().channels.officer || message.channel.id==uhg.getDiscordIds().channels.guild) && message.content.toLowerCase().includes(banned[i])) return message.reply({ content: "Zpráva obsahuje nepovolené slovo" })
   }
 
   let chat;
