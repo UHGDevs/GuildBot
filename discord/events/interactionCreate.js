bcc204ce8b001fc605d4131a3d10ad4a4f975a46
DavidCzPdy <dakolencik@gmail.com>
2022-06-28 18:39:21 +0200

    Improved modals

--- discord/events/interactionCreate.js
+++ discord/events/interactionCreate.js
@@ -1,4 +1,5 @@
 const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
+const fs = require('fs');
 
 module.exports = async (uhg, interaction) => {
   if (interaction.isCommand()) require('../interactions/slashcommands')(uhg, interaction)
@@ -8,6 +9,7 @@ module.exports = async (uhg, interaction) => {
   else if (interaction.isAutocomplete() && interaction.commandName == 'lb') require('../interactions/lb_autocomplete')(uhg, interaction)
   else if (interaction.isAutocomplete() && interaction.commandName == 'cmd') require('../interactions/cmd_autocomplete')(uhg, interaction)
   else if (interaction.isButton() && interaction.customId.startsWith('uhg_rules_')) require('../interactions/uhg_rules')(uhg, interaction)
-  else if (interaction.isButton() && interaction.customId.startsWith('activate_modal_test')) require('../interactions/test')(uhg, interaction)
+  else if (interaction.isButton() && interaction.customId.startsWith('create_modal_')) require(`../interactions/modal/${fs.readdirSync(`discord/interactions/modal`).filter(n => n == interaction.customId.split('_')[2]+'.js')[0]}`).send(uhg, interaction)
+  else if (interaction.isModalSubmit() ) require(`../interactions/modal/${fs.readdirSync(`discord/interactions/modal`).filter(n => n == interaction.customId.split('_')[2]+'.js')[0]}`).get(uhg, interaction)
 
 }
