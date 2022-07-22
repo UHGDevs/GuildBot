2dfa57f925d0b0c414b33494aebc6241889a4b84
DavidCzPdy <dakolencik@gmail.com>
2022-07-22 22:27:45 +0200

    uhg role changes

--- time/events/uhgrole.js
+++ time/events/uhgrole.js
@@ -9,42 +9,31 @@ module.exports = {
   emoji: '🌙',
   time: '30 */5 * * * *', //'*/10 * * * * *'
   ignore: '* * 0,23 * * *', //'sec min hour den(mesic) mesic den(tyden)'
-  onstart: true,
+  onstart: false,
   run: async (uhg) => {
     let date = new Date()
     let event = time.start(uhg, eventName)
     try {
+
+      /* -- Fetch databases -- */
       let dVerify = await uhg.mongo.run.get("general", "verify")
       uhg.data.verify = dVerify
       let dUhg = await uhg.mongo.run.get("general", "uhg")
-      let membersData = await uhg.mongo.run.get("stats", "stats")
-      uhg.data.stats = membersData
-
-      if (date.getHours() == 3 && date.getMinutes() == 0) {
-        for (let verified of dVerify) {
-          console.log(verified)
-          let api = await uhg.getApi(verified.uuid, ['hypixel'])
-          if (api instanceof Object == false) continue
-          if (api.username !== verified.nickname) uhg.mongo.run.update("general", "verify", {_id:verified._id, nickname: api.username, names: api.hypixel.nicks })
-          await uhg.delay(700)
-        }
-        dVerify = await uhg.mongo.run.get("general", "verify")
-        uhg.data.verify = dVerify
-      }
+      uhg.data.uhg = dUhg
+      let dLoot = await uhg.mongo.run.get('general', 'loot')
       
-
+      /* -- Fetch Guild api of Unisdynasty -- */
       let api = await uhg.getApi("64680ee95aeb48ce80eb7aa8626016c7", ["guild"])
-      if (api instanceof Object == false) return console.log(api)
+      if (api instanceof Object == false) throw new Error(api)
 
       let members = api.guild.all.members
 
+
+      /* -- Make sure UHG database is up to date -- */
       let unUuid = []
-      let allUuids = []
-
-
-      /* mongo UHG coordinate */
+      let uhgUuids = []
       for (let member of members) {
-        allUuids.push(member.uuid);
+        uhgUuids.push(member.uuid);
         let vMember = dVerify.filter(n => n.uuid == member.uuid)
         let uMember = dUhg.filter(n => n.uuid == member.uuid)
 
@@ -53,90 +42,58 @@ module.exports = {
         else if (uMember[0].guildrank != member.rank) uhg.mongo.run.update("general", "uhg", {_id:uMember[0]._id, guildrank: member.rank })
         else if (uMember[0].username != vMember[0].nickname) uhg.mongo.run.update("general", "uhg", {_id:uMember[0]._id, username: vMember[0].nickname })
       }
-      dUhg.filter(n => !allUuids.includes(n.uuid)).forEach(notuhg => { uhg.mongo.run.delete("general", "uhg", {_id: notuhg._id})});
+      dUhg.filter(n => !uhgUuids.includes(n.uuid)).forEach(notuhg => { uhg.mongo.run.delete("general", "uhg", {_id: notuhg._id})});
 
-      /* get UNVERIFIED members */
+
+      /* -- Get UNVERIFIED Guild Members -- */
       unNames = []
       for (let uuid of unUuid) {
-        let autoverify = false
         let uApi = await uhg.getApi(uuid, ["mojang", "hypixel", "guild"])
         if (uApi instanceof Object == false) {unNames.push({name:uuid}); continue;}
         let joined = Math.floor((new Date().getTime()-uApi.guild.member.joined)/ 86400000)
-        if (uApi.hypixel.links.DISCORD) {
-          let discord = uhg.dc.client.users.cache.find(n => `${n.username}#${n.discriminator}` == uApi.hypixel.links.DISCORD)
-          if (discord) {
-            await uhg.mongo.run.post("general", "verify", { _id: discord.id, uuid: uApi.uuid, nickname: uApi.username, updated: Number(new Date()) })
-            autoverify = true
-            uhg.dc.cache.channels.get('achat').send(`Autoverified ${uApi.username} - <@${discord.id}> (${uApi.hypixel.links.DISCORD})`)
-          }
-        }
-        unNames.push( {name:uApi.username, joined: joined, date: `<t:${Math.round(uApi.guild.member.joined/1000)}:R>`, lastLogin: uApi.hypixel.lastLogin, lastOnline: `<t:${Math.round(uApi.hypixel.lastLogin/1000)}:R>`, links: uApi.hypixel.links, autoverify: autoverify} )
+        unNames.push( {name:uApi.username, joined: joined, date: `<t:${Math.round(uApi.guild.member.joined/1000)}:R>`, lastLogin: uApi.hypixel.lastLogin, lastOnline: `<t:${Math.round(uApi.hypixel.lastLogin/1000)}:R>`, links: uApi.hypixel.links} )
       }
       uhg.data.unverified = unNames
 
-      /* get UPDATE names of members */
-      if (date.getHours() === 2 && date.getMinutes() === 0) {
-        dUhg = await uhg.mongo.run.get("general", "uhg")
-        for (let member of dUhg) {
-          let mapi = await uhg.getApi(member.uuid, ["mojang"])
-          if (mapi instanceof Object == false) continue;
-          if (mapi.username != member.username) {
-            await uhg.mongo.run.update("general", "uhg", {_id:member._id, username: mapi.username})
-            uhg.mongo.run.update("general", "verify", {_id:member._id, username: mapi.username})
-          }
-        }
-      }
-
-      dUhg = await uhg.mongo.run.get("general", "uhg")
-      uhg.data.uhg = dUhg
 
       let cache = uhg.dc.cache.uhgroles
-      let roleMembers = cache.find(role => role.id == "530504032708460584").role.members // Guild Member
 
-      /* get UNVERIFIED members with default role */
+      /* get UNVERIFIED members with default/guild role */
       dcUnVer = []
-      let verifiedRole = uhg.dc.cache.uhgroles.find(role => role.id == "478816107222925322").role.members // Default
-      for (let member of verifiedRole) {
-        let v = dVerify.filter(n => n._id==member[0])
-        let uVer = dUhg.filter(n => n._id == member._id)
-
-        if (!v.length) {dcUnVer.push(member[1].nickname||member[1].user.username); continue;}
-
-        member = member[1]
-        v = v[0]
-        if (member._roles.includes("530504032708460584") && !uVer) {
-          for (let role of cache) {
-            if (role[1].id == "478816107222925322") continue // Default
-            if (member._roles.includes(role[1].id)) try { await member.roles.remove(role[1].role) } catch (e) {}
-          }
-        }
-      }
 
       /* Refresh roles on uhg dc */
-      let membersUHG = cache.find(role => role.id == "530504032708460584").role.guild.members.cache // Guild Member
-      for (let member of membersUHG) {
-        if (member[1].user.bot) continue;
+      let guild = uhg.dc.client.guilds.cache.get("455751845319802880")
+      let discordMembers =  await guild.members.fetch()
+
+
+      for (let member of discordMembers) {
         member = member[1]
-        //if (member.user.username !== 'DavidCzPdy') continue
+        if (member.user.bot) continue;
+        let verify = dVerify.find(n => n._id == member.id)
+        let uhgD = dUhg.find(n => n._id == member.id) || {}
+       // let loot = dLoot.find(n => n._id == member._id)
         
-        let v = dUhg.filter(n => n._id==member.id)
-        let v2 = dVerify.filter(n => n._id==member.id) 
-        let data = [];
-        if (v2.length) data = membersData.filter(n => n.uuid == v2[0].uuid)
-        if (!data.length) data = [{username: v2[0] ? v2[0].nickname || '':'', language: v2[0] ? v2[0].language || '':''}]
-        data = data[0]
-        if (!v.length || !v2.length) {
-          if (v2.length) await refresh.uhg_refresh(uhg, member, data, {})
-          else {
-            for (let role of cache) {
-              if (role[1].id == "478816107222925322") continue // Default
-              if (member._roles.includes(role[1].id)) try { await member.roles.remove(role[1].role) } catch (e) {}
-            }
+
+        if (!verify) {
+          if (member.roles.cache.get('478816107222925322')) dcUnVer.push(member.nickname||member.user.username)
+          for (let role of cache) {
+            if (role[1].id == '478816107222925322') continue;
+            if (member._roles.includes(role[1].id)) try { await member.roles.remove(role[1]) } catch (e) {} // Delete other guild roles
           }
           continue;
         }
-        v = v[0]
-        await refresh.uhg_refresh(uhg, member, data, v)
+
+        let data = await uhg.mongo.run.get('stats', 'stats', { uuid: verify.uuid })
+        if (data.length) {
+          data = data[0]
+          if (data.username !== verify.nickname) uhg.mongo.run.update("general", "verify", {_id:verify._id, nickname: data.username, names: data.nicks })
+          if (uhgD && data.username !== uhgD.username) uhg.mongo.run.update("general", "uhg", {_id:verify._id, username: data.username })
+        } else {
+          let user = await uhg.mongo.run.get('general', 'verify', { uuid: verify.uuid })
+          data = { username: user[0].nickname }
+        }
+
+        await refresh.uhg_refresh(uhg, member, data, uhgD)
       }
 
     } catch(e) {
