b836fab2518a3e72b924d163700817e8ea4ac1d0
MrDodo9RIDERR <electromans9999@gmail.com>
2022-07-23 13:48:08 +0200

    Update Help.js

--- minecraft/commands/Help.js
+++ minecraft/commands/Help.js
@@ -1,10 +1,17 @@
+const { MessageEmbed } = require("discord.js")
 module.exports = {
   name: "Help",
   aliases: ["help"],
   run: async (uhg, pmsg) => {
     try{
+      console.log(pmsg)
       let message = `[!help] | Ahoj, já jsem UHG Guild BOT, všechny dostupné příkazy najdeš v !commands`
-      return message
+      let embed = new MessageEmbed()
+        .setTitle("**Help**")
+        .setThumbnail(uhg.dc.client.user.displayAvatarURL())
+        .setDescription("Ahoj, já jsem UHG Guild BOT, všechny dostupné příkazy najdeš v !commands")
+        .setColor("0x06ACEE")
+      return {mc: message, dc: embed}
     } catch (e) {
         console.log(String(e.stack).bgRed)
         return "Chyba v Help příkazu!"
