94de3b244068bf47e13f9ab23275e8912aedb9cc
Farmans <Farman52CZ@seznam.cz>
2022-07-23 11:01:37 +0200

    david throwing

--- discord/commandsCmd/profile.js
+++ discord/commandsCmd/profile.js
@@ -31,6 +31,7 @@ module.exports = {
           { name: `Level`, value: `${uhg.f(api.hypixel.level)}`, inline: true },
           { name: `Rank`, value: `${api.hypixel.rank}`, inline: true},
           { name: `Last Login`, value: api.hypixel.lastLogin>0 ? `<t:${Math.round(api.hypixel.lastLogin/1000)}:R>`: '\`api off\`', inline: true},
+          { name: `ㅤ`, value: `ㅤ`, inline: true},
           { name: `User Language`, value: `${api.hypixel.userLanguage}`, inline: true }
       )
 
